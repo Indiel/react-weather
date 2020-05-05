11366d96cd79b785ec8cc1584a8ff9a908c6c5db
Indiel <kori.yanagi@gmail.com>
2020-05-05 22:57:15 +0300

    Updates

---
+++ precache-manifest.d174d1322658d65590c2b278034034ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec7b47a45a80d96f9d10ee9c858f57c9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc89b4889cc7967c4ab4",
+    "url": "./static/css/main.fea92b61.chunk.css"
+  },
+  {
+    "revision": "9cb4a8d978aab7a96d61",
+    "url": "./static/js/2.2ad598bc.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "./static/js/2.2ad598bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc89b4889cc7967c4ab4",
+    "url": "./static/js/main.fd911a96.chunk.js"
+  },
+  {
+    "revision": "8b8d5b8e3726f03428c4",
+    "url": "./static/js/runtime-main.5654c187.js"
+  }
+]);
